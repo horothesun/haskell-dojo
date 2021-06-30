e535d049ac7e74a2fe225ec1c8a413a283202442
horothesun <horothesun@gmail.com>
2021-06-30 22:40:36 +0100

    StateLib: cleanup

--- src/StateLib.hs
+++ src/StateLib.hs
@@ -12,15 +12,10 @@ type Followers = M.Map UserId (NS.NESet UserId)
 saveFollow :: UserId -> UserId -> State Followers ()
 saveFollow follower user = state $ \fs -> ((), saveFollow' follower user fs)
 
-saveFollow' :: UserId -> UserId -> Followers -> Followers
-saveFollow' follower user fs = M.insert user newUserFollowers fs
-  where
-    oldUserFollowersMaybe = M.lookup user fs
-    newUserFollowers = maybe (NS.singleton follower) (NS.insert follower) oldUserFollowersMaybe
-
 saveUnfollow :: UserId -> UserId -> State Followers ()
 saveUnfollow follower user = state $ \fs -> ((), saveUnfollow' follower user fs)
 
+-- O(log n)
 saveUnfollow' :: UserId -> UserId -> Followers -> Followers
 saveUnfollow' follower user fs = maybe fs removeFollower oldUserFollowersMaybe
   where
@@ -29,3 +24,10 @@ saveUnfollow' follower user fs = maybe fs removeFollower oldUserFollowersMaybe
     removeFollower nes = if isOnlyFollower nes
       then M.delete user fs
       else M.insert user (NS.unsafeFromSet $ NS.delete follower nes) fs
+
+-- O(log n)
+saveFollow' :: UserId -> UserId -> Followers -> Followers
+saveFollow' follower user fs = M.insert user newUserFollowers fs
+  where
+    oldUserFollowersMaybe = M.lookup user fs
+    newUserFollowers = maybe (NS.singleton follower) (NS.insert follower) oldUserFollowersMaybe
